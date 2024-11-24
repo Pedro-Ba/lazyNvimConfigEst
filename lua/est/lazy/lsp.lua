85e4257431c863af4d7822df3c4e830c07e0271f
Pedro-Ba <pedropaschbarraza@gmail.com>
2024-11-24 12:43:01 -0300

    FEAT: Add cmp-path and signature, change mappings

--- lua/est/lazy/lsp.lua
+++ lua/est/lazy/lsp.lua
@@ -5,6 +5,8 @@ return {
         "williamboman/mason-lspconfig.nvim",
         "hrsh7th/cmp-nvim-lsp",
         "hrsh7th/nvim-cmp",
+        "hrsh7th/cmp-path",
+        "hrsh7th/cmp-nvim-lsp-signature-help",
     },
 
     config = function()
@@ -28,13 +30,15 @@ return {
                 documentation = cmp.config.window.bordered()
             },
             mapping = cmp.mapping.preset.insert({
-                ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-                ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-                ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+                ['<C-k>'] = cmp.mapping.select_prev_item(cmp_select),
+                ['<C-j>'] = cmp.mapping.select_next_item(cmp_select),
+                ['<CR>'] = cmp.mapping.confirm({ select = true }),
                 ["<C-Space>"] = cmp.mapping.complete(),
             }),
             sources = {
-                { name = 'nvim_lsp' }
+                { name = 'nvim_lsp' },
+                { name = 'path' },
+                { name = 'nvim_lsp_signature_help' }
             }
         })
     end
