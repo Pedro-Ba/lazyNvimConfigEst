433c4431d0ff9400b9cadab044350933e0933d27
Pedro-Ba <pedropaschbarraza@gmail.com>
2024-12-12 08:53:52 -0300

    Feat: add new colorschemes, change default colorscheme

--- lua/est/lazy/colors.lua
+++ lua/est/lazy/colors.lua
@@ -3,14 +3,14 @@ return {
     name = "catppuccin",
     lazy = false,
     priority = 1000,
+    dependencies = {
+        { "rose-pine/neovim", name = "rose-pine" },
+        "AlexvZyl/nordic.nvim",
+ --       "scottmckendry/cyberdream.nvim",
+        "rebelot/kanagawa.nvim",
+        "folke/tokyonight.nvim"
+    },
     config = function()
-        vim.cmd.colorscheme('catppuccin')
+        vim.cmd.colorscheme('kanagawa')
     end
-	-- "rose-pine/neovim", 
-	-- name = "rose-pine",
-	-- lazy = false,
-	-- priority = 1000,
-	-- config = function()
-	-- 	vim.cmd.colorscheme('rose-pine')
-	-- end
 }
--- lua/est/lazy/lualine.lua
+++ lua/est/lazy/lualine.lua
@@ -4,7 +4,7 @@ return {
         require("lualine").setup({
             options = {
                 icons_enabled = false,
-                theme = 'catppuccin',
+ --               theme = 'catppuccin',
                 ignore_focus = {},
                 always_divide_middle = true,
                 globalstatus = true,
